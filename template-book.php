31dd1bccb521e4a1e2aeded677f5bb1405c300b5
guilhem <guilhem.menard@gmail.com>
2017-05-16 19:16:24 +0200

    modification du template book et de la feuille de style

--- template-book.php
+++ template-book.php
@@ -7,34 +7,28 @@
     */
 
 get_header();
-// Get theme options
-$enable_parallax = get_theme_mod('themeora_enable_parallax', 'yes');
-$parallax_settings = '';
-if ( 'yes' === $enable_parallax ){
-    //advanced users can change this line to change the parallax effect. For documentation check https://github.com/Prinzhorn/skrollr
-    $parallax_settings = 'data-center="background-position: 50% 50%;"
-    data-top-bottom="background-position: 50% 20%;"';
-}
-
-// Get the featured image to set as the background of the header. Use header_image if set, featured image if not
 ?>
 
-<header >
-	<div class="container">
-    <h1 class="title"><span><?php the_title(); ?></span></h1>
-	</div>
-</header>
 
 <div class="full-width-container main-content-area">
     
     <div class="container">
         <div class="row">
-            <div class="col-md-4"><?php 
+            <div class="col-md-3 img-responsive center-block"><?php 
 			if ( has_post_thumbnail() ) {
-			the_post_thumbnail();
+			the_post_thumbnail( array(800,3200) );
 			}  ?>
 			</div>
-            <div class="col-md-8"><?php the_meta(); ?></div>
+            	<div class="col-md-8">
+            		<h1 class="booktitle">Titre du livre : <?php the_field('title'); ?> </br>
+            	    Auteur : <?php the_field('author'); ?></h1>
+            		<p class="summary"><b>Résumé du livre : </b><?php the_field('summary'); ?></p>
+            		<p class="rec"><b>Recommandation de Steve : </b><?php the_field('rec'); ?></p>
+            		<h4> ACHETER LE LIVRE SUR : </h4>
+            	    <a href="<?php the_field('amazon'); ?>"> <img src="<?php the_field('buy1'); ?>" alt="" /> </a>
+            	    <a href="<?php the_field('leslibraires'); ?>"> <img src="<?php the_field('buy2'); ?>" alt="" /> </a>
+            	    <a href="<?php the_field('fnac'); ?>"> <img src="<?php the_field('buy3'); ?>" alt="" /> </a>
+				</div>
             <?php the_content(); ?>
             <div class="content">
             </div> <!-- end row -->
